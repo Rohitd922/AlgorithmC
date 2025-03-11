011b5fcb581897290aa7f342cb520a973e411a73
rohit922 <dube.rohit@tamu.edu>
2025-03-11 16:42:40 -0500

    update gcd

--- euclids algorithm/gcd.c
+++ euclids algorithm/gcd.c
@@ -8,3 +8,11 @@ int gcd(int a, int b) {
     }
     return gcd(b, a % b);
 }
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("GCD of %d and %d is %d\n", a, b, gcd(a, b));
+    return 0;
+}
